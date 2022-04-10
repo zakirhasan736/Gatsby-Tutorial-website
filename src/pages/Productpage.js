f1b0ed9ca0bef4268ea60e8fd35a6f3ef6626f12
unknown <zakirhosaib736@gmail.comm>
2022-04-10 11:34:33 +0600

    updates

---
+++ src/pages/Productpage.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Header from "../components/Header/Header"
+
+
+
+const Productpage = () => (
+  <Layout >
+    <SEO title="Home" />
+   <Header  />
+      
+  </Layout>
+)
+
+export default Productpage
